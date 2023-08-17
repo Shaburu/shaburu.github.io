b6d2f37aeb52830699824e21d5e38f840dcdb440
shaburu <xshaburu@gmail.com>
2023-08-17 09:40:11 +0530

    just to throw stupid errors

--- vxr/src/components/Navbar.tsx
+++ vxr/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef} from "react";
 
 import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
@@ -6,17 +6,38 @@ import { Link, NavLink } from "react-router-dom";
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  
+  let menuRef = useRef();
+  let menuRef2 = useRef();
+
+  useEffect(() =>{
+    let handler =(e)=>{
+      if(!menuRef2.current.contains(e.target) && !menuRef.current.contains(e.target)){
+        setMenuOpen(false);
+        console.log("!menuRef.current.contains(e.target)")
+        
+      }
+    };
+    document.addEventListener('mousedown',handler)
+
+    return() =>{
+      document.removeEventListener('mousedown',handler)
+      console.log("document.removeEventListener('mousedown',handler)")
+
+    }
+  });
+
   return (
     <nav>
       <Link to="/" className="title">
         SHABURU
       </Link>
-      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+      <div className="menu" ref={menuRef2} onClick={() => setMenuOpen(!menuOpen)}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <ul className={menuOpen ? "open" : ""}>
+      <ul className={menuOpen ? "open" : ""} ref={menuRef} >
         <li>
           <NavLink to="/about">BLOG</NavLink>
         </li>
